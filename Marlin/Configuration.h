625d6efa950961ff3ad83a056763b180a4ba249b
thecount2255 <allmine2255@gmail.com>
2022-04-19 21:48:04 -0500

    Small change to FR/ACC/Jerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -934,7 +934,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 1000, 1000, 20, 100 } //%% Default? = 8000, 8000, 15, 15000
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 10, 50 } //%% Default? = 8000, 8000, 15, 15000
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -947,7 +947,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 } //%% Default? = 5000, 5000, 100, 10000
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 10, 50 } //%% Default? = 5000, 5000, 100, 10000
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
